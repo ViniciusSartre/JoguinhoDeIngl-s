01ba4d859eb52b6faa8c6bfaf3834a688c806bfd
ViniciusSartre <vinicius_sartre@hotmail.com>
2024-11-29 18:17:45 -0300

    AlertaCadastro

--- Joguinho/src/components/formulario.vue
+++ Joguinho/src/components/formulario.vue
@@ -1,124 +1,156 @@
 <template>
-    <div class="cadastro">
-      <img src="https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/1507580/ss_8db1997119b44bf18e6c41d72b696b097210ce8d.1920x1080.jpg?t=1732837719" alt="">
-      <div class="caixaconteudo">
-        <div class="titulo">
-          <h1>Guess!</h1>
-        </div>
-        <div class="subtitulo">
-          <h2>Cadastre os Jogadores</h2>
-        </div>
-        <form @submit.prevent="irParaProximaEtapa">
-          <div class="jogador" v-for="(jogador, index) in jogadores" :key="index">
-            <label>
-              Jogador {{ index + 1 }}
-              <input
-                type="text"
-                v-model="jogadores[index]"
-                :placeholder="'Jogador ' + (index + 1)"
-              />
-            </label>
-          </div>
-          <div class="botao">
-            <input type="submit" value="JOGAR" />
-          </div>
-        </form>
+  <div class="cadastro">
+    <img
+      src="https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/1507580/ss_8db1997119b44bf18e6c41d72b696b097210ce8d.1920x1080.jpg?t=1732837719"
+      alt=""
+    />
+    <div class="caixaconteudo">
+      <div class="titulo">
+        <h1>Guess!</h1>
       </div>
+      <div class="subtitulo">
+        <h2>Cadastre os Jogadores</h2>
+      </div>
+      <form @submit.prevent="irParaProximaEtapa">
+        <div class="jogador" v-for="(jogador, index) in jogadores" :key="index">
+          <label>
+            Jogador {{ index + 1 }}
+            <input
+              type="text"
+              v-model="jogadores[index]"
+              :placeholder="'Jogador ' + (index + 1)"
+            />
+          </label>
+        </div>
+        <div class="botao">
+          <input type="submit" value="JOGAR" />
+        </div>
+      </form>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    name: "Formulario",
-    data() {
-      return {
-        jogadores: ["", "", "", ""],
-      };
-    },
-    methods: {
-      irParaProximaEtapa() {
-        console.log("Jogadores cadastrados:", this.jogadores);
-        // alert("Jogadores cadastrados com sucesso: " + this.jogadores.join(", "));
-        localStorage.setItem("jogadores", JSON.stringify(this.jogadores));
-        this.$router.push('/tabuleiro')
-      },
-    },
-  };
-  </script>
-  
-  <style scoped>
-  .cadastro {
-    display: flex;
-    position: relative;
-    flex-direction: column;
-    height: 100vh;
-    /* background-color: gray; */
-    color: white;
-    align-content: center;
 
-    img{
-      filter: blur(5px);
-      display: block;
-      max-width: 100vw;
-      max-height: 100vh;
-    }
-  }
-  
-  .caixaconteudo {
-    position: absolute;
-    width: 30%;
-    margin-top: 20vh;
-    padding: 20px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    border-radius: 50px;
-    background-color: #a9a9a9cb;
-    align-self: center;
-  }
-  
-  .titulo {
-    font-size: 25px;
-    margin: 0 0 30px 0;
-  }
-  
-  .subtitulo {
-    font-size: 20px;
-    margin: 0 0 10px 0;
-  }
-  
-  .jogador {
-    margin-bottom: 10px;
-  }
-  
-  .jogador label {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
-  
-  .jogador input {
-    border: none;
-    margin: 5px;
-    padding: 12px;
-    border-radius: 30px;
-  }
-  
-  .botao {
-    display: flex;
-    justify-content: center;
-  }
-  
-  .botao input {
-    margin: 10px;
-    padding: 10px;
-    border: none;
-    border-radius: 5px;
-    cursor: pointer;
-  }
+    <div v-if="mensagemErro" class="mensagem-erro">
+      <p>{{ mensagemErro }}</p>
+    </div>
+  </div>
+</template>
 
-  .botao input:hover {
-    background-color: black;
-    color: white;
+<script>
+export default {
+  name: "Formulario",
+  data() {
+    return {
+      jogadores: ["", "", "", ""],
+      mensagemErro: "",
+    };
+  },
+  methods: {
+    irParaProximaEtapa() {
+      const jogadoresValidos = this.jogadores.filter(
+        (jogador) => jogador.trim() !== ""
+      );
+
+      if (jogadoresValidos.length < 2) {
+        this.mensagemErro = "Por favor, cadastre pelo menos dois jogadores.";
+        setTimeout(() => {
+          this.mensagemErro = "";
+        }, 3000);
+        return;
+      }
+
+      localStorage.setItem("jogadores", JSON.stringify(jogadoresValidos));
+      this.$router.push("/tabuleiro");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.cadastro {
+  display: flex;
+  position: relative;
+  flex-direction: column;
+  height: 100vh;
+  color: white;
+  align-content: center;
+
+  img {
+    filter: blur(5px);
+    display: block;
+    max-width: 100vw;
+    max-height: 100vh;
   }
-  </style>
+}
+
+.caixaconteudo {
+  position: absolute;
+  width: 30%;
+  margin-top: 20vh;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border-radius: 50px;
+  background-color: #a9a9a9cb;
+  align-self: center;
+}
+
+.titulo {
+  font-size: 25px;
+  margin: 0 0 30px 0;
+}
+
+.subtitulo {
+  font-size: 20px;
+  margin: 0 0 10px 0;
+}
+
+.jogador {
+  margin-bottom: 10px;
+}
+
+.jogador label {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.jogador input {
+  border: none;
+  margin: 5px;
+  padding: 12px;
+  border-radius: 30px;
+}
+
+.botao {
+  display: flex;
+  justify-content: center;
+}
+
+.botao input {
+  margin: 10px;
+  padding: 10px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+.botao input:hover {
+  background-color: black;
+  color: white;
+}
+
+
+.mensagem-erro {
+  position: fixed;
+  top: 20px;
+  left: 50%;
+  transform: translateX(-50%);
+  background-color: red;
+  padding: 15px 25px;
+  border-radius: 10px;
+  color: white;
+  font-weight: bolder;
+  text-align: center;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+}
+</style>
